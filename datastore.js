05aa7f48f20e315c09a639783515d5ee4a9e57f0
nidhin28 <nidhinfullstack@gmail.com>
2018-03-29 13:07:07 -0400

    First commit Quiz

---
+++ datastore.js
@@ -0,0 +1,122 @@
+const STORE = [
+    {
+      question: 'Which U.S. state, nicknamed “the Sunflower State,” has the native sunflower as its state flower?',
+      answers: [
+        'Maryland',
+        'Arizona',
+        'Oklahoma',
+        'Kansas'
+        ],
+      correctAnswer: 'Kansas',
+      icon: 'thumbsup.png',
+        alt: 'Thumps up'
+    },
+    {
+      question: 'The Yellow Crane Tower, which is the symbol of Wuhan City, is located in the Hubei Province of what country?',
+      answers: [
+        'Niger',
+        'China',
+        'Nigeria',
+        'Vietnam'
+        ],
+        correctAnswer: 'China',
+        icon: 'thumbsup.png',
+        alt: 'Thumps up'
+    },
+    {
+      question: 'The Gold Coast used to be the name for what country on Africa’s west coast?',
+      answers: [
+        'Nigeria',
+        'Togo',
+        'Ivory Coast',
+        'Ghana'
+        ],
+        correctAnswer: 'Ghana',
+        icon: 'thumbsup.png',
+        alt: 'Thumps up'
+    },
+    {
+      question: 'Which african country capital is known as new flower?',
+      answers: [
+        'Tanzania',
+        'Niger',
+        'Uganda',
+        'Ethiopia'
+        ],
+      correctAnswer: 'Ethiopia',
+      icon: 'thumbsup.png',
+      alt: 'thumps up'
+    },
+    {
+      question: 'City of Barcelona is located in which country ?',
+      answers: [
+        'France',
+        'Romania',
+        'Italy',
+        'Spain'
+        ],
+      correctAnswer: 'Spain',
+      icon: 'thumbsup.png',
+      alt: 'Thumps up'
+    },
+    {
+      question: 'Which city is badly damaged in 1964 eart quake and now the most populated city in alaska?',
+      answers: [
+        'Pole star City',
+        'Fairbanks',
+        'Carson City',
+        'Anchorage'
+        ],
+      correctAnswer: 'Anchorage',
+      icon: 'thumbsup.png',
+      alt: 'Thumps up'
+    },
+    {
+      question: 'City which connects Europe to asia minor ?',
+      answers: [
+      'Moscow',
+      'Ekaterinaburg',
+      'Istanbul',
+      'Yerevan'
+      ],
+      correctAnswer: 'Istanbul',
+      icon: 'thumbsup.png',
+      alt: 'Thumps up'
+    },
+    {
+      question: 'Largest city in Republic of Ireland ?',
+      answers: [
+        'Belfast',
+        'Cardiff',
+        'Glascow',
+        'Dublin'
+      ],
+      correctAnswer: 'Dublin',
+      icon: 'thumbsup.png',
+      alt: 'thumps up'
+    },
+    {
+      question: 'Most populated  State in USA ?',
+      answers: [
+      'Texas',
+      'Louisiana',
+      'California',
+      'Virginia'
+      ],
+      correctAnswer: 'California',
+      icon: 'thumbsup.png',
+      alt: 'thumps up'
+    },
+    {
+      question: 'Which country is named after Equator?',
+      answers: [
+        'Uruguay',
+        'Paraguay',
+        'Ecuador',
+        'Colombia'
+    ],
+    correctAnswer: 'Ecuador',
+    icon: 'thumbsup.png',
+    alt: 'Thumps up'
+    }
+];
